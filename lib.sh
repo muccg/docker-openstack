db2a7463fb0b3063604ea7fcf6163be5c73bf3ce
aahunter <ahunter@ccg.murdoch.edu.au>
2016-09-27 10:54:55 +0800

    remove bamboo references

--- lib.sh
+++ lib.sh
@@ -114,17 +114,6 @@ _pip_proxy() {
 
 _ci_docker_login() {
     info 'Docker login'
-
-    if [ -z ${DOCKER_EMAIL+x} ]; then
-        DOCKER_EMAIL=${bamboo_DOCKER_EMAIL}
-    fi
-    if [ -z ${DOCKER_USERNAME+x} ]; then
-        DOCKER_USERNAME=${bamboo_DOCKER_USERNAME}
-    fi
-    if [ -z ${DOCKER_PASSWORD+x} ]; then
-        DOCKER_PASSWORD=${bamboo_DOCKER_PASSWORD}
-    fi
-
-    docker login -e "${DOCKER_EMAIL}" -u ${DOCKER_USERNAME} --password="${DOCKER_PASSWORD}"
+    docker login -u ${DOCKER_USERNAME} --password="${DOCKER_PASSWORD}"
     success "Docker login"
 }
